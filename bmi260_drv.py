4e38fec31b3f86e842888f753197e3b3da2227ed
Oleg Sayakhov <olegsajaxov@yandex.ru>
2023-12-09 18:27:45 +0500

    sensor: reinit after system suspend virtual_pointer: refactored toggler: simplified json: added `enable` entry

--- bmi260_drv.py
+++ bmi260_drv.py
@@ -6,13 +6,56 @@ from bmi260 import definitions
 from bmi260.BMI270 import BMI270
 from bmi260.config_file import bmi260_config_file
 
+from dataclasses import dataclass
 from libevdev import Device, InputEvent, EV_REL, EV_KEY, EV_ABS, EV_SYN
 
 
+CFG_UPDATE_DELAY = 0.5
+
+def get_sensor():
+    # init
+    sensor = BMI270(registers.I2C_SEC_ADDR)
+    sensor.soft_reset()
+    sensor.load_config_file(bmi260_config_file)
+
+    # config
+    sensor.set_mode(definitions.PERFORMANCE_MODE)
+
+    # sensor.set_acc_range(definitions.ACC_RANGE_2G)
+    # sensor.set_acc_odr(definitions.ACC_ODR_200)
+    # sensor.set_acc_bwp(definitions.ACC_BWP_OSR4)
+
+    sensor.set_gyr_range(definitions.GYR_RANGE_2000)
+    sensor.set_gyr_odr(definitions.GYR_ODR_200)
+    sensor.set_gyr_bwp(definitions.GYR_BWP_OSR4)
+
+    sensor.disable_fifo_header()
+    sensor.enable_data_streaming()
+    # sensor.enable_acc_filter_perf()
+    sensor.enable_gyr_noise_perf()
+    sensor.enable_gyr_filter_perf()
+
+    sensor.disable_acc()
+    sensor.disable_aux()
+    sensor.enable_gyr()
+
+    return sensor
+
+
+@dataclass
+class VirtualPointerData:
+    prev: int
+    curr: float
+    changed: bool
+
+@dataclass
+class VirtualPointer:
+    x: VirtualPointerData
+    y: VirtualPointerData
+
+
 # init
-sensor = BMI270(registers.I2C_SEC_ADDR)
-sensor.soft_reset()
-sensor.load_config_file(bmi260_config_file)
+sensor = get_sensor()
 
 gyro_dev = Device()
 gyro_dev.name = 'BMI260 gyro'
@@ -28,46 +71,35 @@ gyro_inp = gyro_dev.create_uinput_device()
 print('new uinput test device at {}'.format(gyro_inp.devnode))
 
 # config
-sensor.set_mode(definitions.PERFORMANCE_MODE)
-
-# sensor.set_acc_range(definitions.ACC_RANGE_2G)
-# sensor.set_acc_odr(definitions.ACC_ODR_200)
-# sensor.set_acc_bwp(definitions.ACC_BWP_OSR4)
-
-sensor.set_gyr_range(definitions.GYR_RANGE_2000)
-sensor.set_gyr_odr(definitions.GYR_ODR_200)
-sensor.set_gyr_bwp(definitions.GYR_BWP_OSR4)
-
-sensor.disable_fifo_header()
-sensor.enable_data_streaming()
-# sensor.enable_acc_filter_perf()
-sensor.enable_gyr_noise_perf()
-sensor.enable_gyr_filter_perf()
-
-sensor.disable_acc()
-sensor.disable_aux()
-sensor.enable_gyr()
-
 last_cfg_read_time = time()
 cfg = json.load(open('/home/arch/GPD/BMI260/gyro.json'))
 
-pos = [[0, 0, False], [0, 0, False]]
+virt_ptr = VirtualPointer(
+    VirtualPointerData(0, 0, False),
+    VirtualPointerData(0, 0, False)
+)
+
 vel = [0, 0]
 
 # run
 while True:
     # load config
-    if time() - last_cfg_read_time > 1:
+    if time() - last_cfg_read_time > CFG_UPDATE_DELAY:
         try:
             cfg = json.load(open('/home/arch/GPD/BMI260/gyro.json'))
             last_cfg_read_time = time()
             # print(cfg)
         except:
-            sleep(1)
+            sleep(CFG_UPDATE_DELAY)
             continue
+        
+        # check sensor
+        if sensor.read_register(registers.INTERNAL_STATUS) == 0:
+            sensor = get_sensor()
+            print('sensor reinitialized')
 
-    if cfg['mode'] != 'mouse':
-        sleep(1)
+    if (not cfg['enable']) or cfg['mode'] != 'mouse':
+        sleep(CFG_UPDATE_DELAY)
         continue
 
     planes = {
@@ -90,24 +122,24 @@ while True:
     vel[0] += cfg['sens'] * data[plane[0]]
     vel[1] += cfg['sens'] * data[plane[1]]
 
-    pos[0][1] -= vel[0] if abs(vel[0]) > 0.05 else 0
-    pos[0][2] = True if abs(pos[0][0] - pos[0][1]) >= 1 else False
+    virt_ptr.x.curr -= vel[0] if abs(vel[0]) > 0.05 else 0
+    virt_ptr.x.changed = True if abs(virt_ptr.x.curr - virt_ptr.x.prev) >= 1 else False
 
-    pos[1][1] -= vel[1] if abs(vel[1]) > 0.05 else 0
-    pos[1][2] = True if abs(pos[1][0] - pos[1][1]) >= 1 else False
+    virt_ptr.y.curr -= vel[1] if abs(vel[1]) > 0.05 else 0
+    virt_ptr.y.changed = True if abs(virt_ptr.y.curr - virt_ptr.y.prev) >= 1 else False
 
-    # print(f'{data} -> {vel} -> {pos}')
+    # print(f'{data} -> {vel} -> {virt_ptr}')
 
     # send events
     events = [
-        InputEvent(EV_REL.REL_X, (int(pos[0][1]) - pos[0][0]) if pos[0][2] else 0),
-        InputEvent(EV_REL.REL_Y, (int(pos[1][1]) - pos[1][0]) if pos[1][2] else 0),
+        InputEvent(EV_REL.REL_X, (int(virt_ptr.x.curr) - virt_ptr.x.prev) if virt_ptr.x.changed else 0),
+        InputEvent(EV_REL.REL_Y, (int(virt_ptr.y.curr) - virt_ptr.y.prev) if virt_ptr.y.changed else 0),
         InputEvent(EV_SYN.SYN_REPORT, 0)
     ]
     gyro_inp.send_events(events)
 
-    if abs(pos[0][0] - pos[0][1]) >= 1:
-        pos[0][0] = int(pos[0][1])
+    if abs(virt_ptr.x.curr - virt_ptr.x.prev) >= 1:
+        virt_ptr.x.prev = int(virt_ptr.x.curr)
 
-    if abs(pos[1][0] - pos[1][1]) >= 1:
-        pos[1][0] = int(pos[1][1])
+    if abs(virt_ptr.y.curr - virt_ptr.y.prev) >= 1:
+        virt_ptr.y.prev = int(virt_ptr.y.curr)
--- toggle.py
+++ toggle.py
@@ -1,11 +1,6 @@
 import json
 
 
-table = {
-    None: 'mouse',
-    'mouse': None
-}
-
 cfg = json.load(open('/home/arch/GPD/BMI260/gyro.json'))
-cfg['mode'] = table[cfg['mode']]
+cfg['enable'] = not cfg['enable']
 json.dump(cfg, open('/home/arch/GPD/BMI260/gyro.json', 'w'))
