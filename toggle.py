b552536cde3589ba9b4bf410d8296d1231dbe42e
Oleg Sayakhov <olegsajaxov@yandex.ru>
2023-12-14 00:21:49 +0500

    toggler: change dpi

--- toggle.py
+++ toggle.py
@@ -1,9 +1,22 @@
 import json
+import argparse
 
-
+# config
 GYRO_CFG_PATH = '/home/arch/GPD/BMI260/gyro.json'
-
 cfg = json.load(open(GYRO_CFG_PATH))
 
-cfg['enable'] = not cfg['enable']
+dpi = [0.28, 0.42, 0.64, 0.8, 1.0]
+
+parser = argparse.ArgumentParser('Gyro toggler')
+parser.add_argument('--dpi', action='store_true')
+
+# run
+args = parser.parse_args()
+
+if args.dpi:
+    cfg['sens'] = next((v for v in dpi if cfg['sens'] < v), dpi[0])
+else:
+    cfg['enable'] = not cfg['enable']
+
 json.dump(cfg, open(GYRO_CFG_PATH, 'w'))
+
